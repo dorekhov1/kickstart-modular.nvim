63ea7b00a93494f82df050429180bceb0554ff1c
Daniil Orekhov <daniil.orekhov15@gmail.com>
2024-02-12 21:52:27 +0300

    my first setup wip

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -25,4 +25,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- Paste by replace
+vim.keymap.set('v', '<leader>p', '"_dP', { desc = "Replace with P" })
+
+-- Copy to system clipboard
+vim.keymap.set({ 'v', 'n' }, '<leader>y', '"+y', { desc = "Copy to system clipboard" })
+
+-- Copy entire file
+vim.keymap.set('n', '<leader>Y', 'gg"+yG', { desc = "Copy the entire file to system clipboard" })
+
+-- Move selection around
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 -- vim: ts=2 sts=2 sw=2 et
